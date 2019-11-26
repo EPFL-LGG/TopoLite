26cc7fc1f75014d7fd77e6197563e0eb3975a378
Ziqi Wang <qiqiustc@gmail.com>
2019-11-26 17:05:01 +0100

    update

--- build/ext/nanogui/CMakeFiles/nanogui.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libnanogui.a"
-  "libnanogui.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C CXX)
-  include(CMakeFiles/nanogui.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/ext/nanogui/ext_build/glfw/src/CMakeFiles/glfw.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libglfw3.a"
-  "libglfw3.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/glfw.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
