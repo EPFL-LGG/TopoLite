fe54ef19bed941cd689f5401a48edbaab05bfda5
Ziqi Wang <qiqiustc@gmail.com>
2019-12-13 17:22:28 +0100

    Update XMLIO.cpp

--- src/TopoLite/test/XMLIO.cpp
+++ src/TopoLite/test/XMLIO.cpp
@@ -17,16 +17,18 @@ TEST_CASE("Class XMLIO")
         XMLIO Reader;
         XMLData data;
 
-    boost::filesystem::path current_path(boost::filesystem::current_path());
-    boost::filesystem::path debugxml_filepath;
-    if(current_path.filename() == "TopoLite"){
-        debugxml_filepath = current_path / "data/TopoInterock/XML/origin.xml";
-    }
-    else{
-        debugxml_filepath = current_path / "../data/TopoInterock/XML/origin.xml";
-    }
+        boost::filesystem::path current_path(boost::filesystem::current_path());
+        boost::filesystem::path debugxml_filepath;
+        if (current_path.filename() == "TopoLite")
+        {
+            debugxml_filepath = current_path / "data/TopoInterock/XML/origin.xml";
+        }
+        else
+        {
+            debugxml_filepath = current_path / "../data/TopoInterock/XML/origin.xml";
+        }
 
-    REQUIRE(Reader.XMLReader(debugxml_filepath.string(), data) == 1);
-    Reader.XMLWriter(debugxml_filepath.string(), data);
+        REQUIRE(Reader.XMLReader(debugxml_filepath.string(), data) == 1);
+        Reader.XMLWriter(debugxml_filepath.string(), data);
     }
 }
