91c1b25ee18510e840928049671ab7998037957c
rjodon <robin.jodon@epfl.ch>
2020-03-26 15:53:39 +0100

    Reformat code
    Reformatting performed automatically

--- src/TopoLite/Interlocking/ContactGraph.cpp
+++ src/TopoLite/Interlocking/ContactGraph.cpp
@@ -13,23 +13,22 @@ template<typename Scalar>
 ContactGraph<Scalar>::ContactGraph(const shared_ptr<InputVarList>& varList):TopoObject(varList) {}
 
 template<typename Scalar>
-ContactGraph<Scalar>::~ContactGraph()
-{
+ContactGraph<Scalar>::~ContactGraph() {
     nodes.clear();
     edges.clear();
 }
 
 template<typename Scalar>
 bool ContactGraph<Scalar>::constructFromPolyMeshes(vector<pPolyMesh> &meshes,
-                                           vector<bool> &atBoundary,
-                                           double eps)
-{
+                                                   vector<bool> &atBoundary,
+                                                   double eps) {
 
     // 0) scale the meshes into united box
     double maxD = 1;
     for (size_t id = 0; id < meshes.size(); id++) {
         pPolyMesh poly = meshes[id];
-        if (poly == nullptr) return false;
+        if (poly == nullptr)
+            return false;
         for (pPolygon face : poly->polyList) {
             // 1.1) construct plane
             plane_contact plane;
@@ -38,7 +37,7 @@ bool ContactGraph<Scalar>::constructFromPolyMeshes(vector<pPolyMesh> &meshes,
             plane.nrm = nrm;
 
             plane.D = nrm.dot(center);
-            maxD = (std::max)(maxD, (double)std::abs(plane.D));
+            maxD = (std::max)(maxD, (double) std::abs(plane.D));
         }
     }
 
@@ -47,21 +46,20 @@ bool ContactGraph<Scalar>::constructFromPolyMeshes(vector<pPolyMesh> &meshes,
     std::set<plane_contact, plane_contact_compare> setPlanes;
 
     int groupID = 0;
-    for (size_t id = 0; id < meshes.size(); id++)
-    {
+    for (size_t id = 0; id < meshes.size(); id++) {
         pPolyMesh poly = meshes[id];
         if (poly == nullptr)
             return false;
-        for (pPolygon face : poly->polyList)
-        {
-            if(face->vers.size() < 3) continue;
+        for (pPolygon face : poly->polyList) {
+            if (face->vers.size() < 3)
+                continue;
 
             // 1.1) construct plane
             plane_contact plane;
             Vector3 nrm = face->normal();
             Vector3 center = face->vers[0]->pos;
             plane.nrm = Vector3(nrm[0], nrm[1], nrm[2]);
-            
+
             plane.D = nrm.dot(center) / maxD;
             plane.partID = id;
             plane.polygon = face;
@@ -69,124 +67,115 @@ bool ContactGraph<Scalar>::constructFromPolyMeshes(vector<pPolyMesh> &meshes,
 
             // 1.2) find groupID
             typename std::set<plane_contact, plane_contact_compare>::iterator find_it = setPlanes.end();
-            for(int reverse = -1; reverse <= 1; reverse += 2){
+            for (int reverse = -1; reverse <= 1; reverse += 2) {
                 plane_contact tmp_plane = plane;
                 tmp_plane.nrm *= reverse;
                 tmp_plane.D *= reverse;
                 find_it = setPlanes.find(tmp_plane);
-                if(find_it != setPlanes.end()){
+                if (find_it != setPlanes.end()) {
                     plane.groupID = (*find_it).groupID;
                     break;
                 }
             }
 
-            if(find_it == setPlanes.end()){
-                plane.groupID = groupID ++;
+            if (find_it == setPlanes.end()) {
+                plane.groupID = groupID++;
                 setPlanes.insert(plane);
             }
-            
+
             planes.push_back(plane);
         }
     }
 
-    std::sort(planes.begin(), planes.end(), [&](const plane_contact &A, const plane_contact &B){
+    std::sort(planes.begin(), planes.end(), [&](const plane_contact &A, const plane_contact &B) {
         return A.groupID < B.groupID;
     });
 
-     // 2) add nodes
-     for(size_t id = 0; id < meshes.size(); id++){
-         Vector3 centroid = meshes[id]->centroid();
-         Scalar volume = meshes[id]->volume();
-         pContactGraphNode node = make_shared<ContactGraphNode<Scalar>>(atBoundary[id], centroid, centroid, volume);
-         addNode(node);
-     }
+    // 2) add nodes
+    for (size_t id = 0; id < meshes.size(); id++) {
+        Vector3 centroid = meshes[id]->centroid();
+        Scalar volume = meshes[id]->volume();
+        pContactGraphNode node = make_shared<ContactGraphNode<Scalar>>(atBoundary[id], centroid, centroid, volume);
+        addNode(node);
+    }
 
-     // 3) find all pairs of contact polygon
-     int sta = 0, end = 0;
-     vector<pairIJ> planeIJ;
-     while(sta < planes.size())
-     {
-          for(end = sta + 1; end < planes.size(); end++)
-          {
-              if(planes[sta].groupID != planes[end].groupID)
-              {
-                  break;
-              }
-          }
-          if (end - sta > 1)
-          {
-              for (int id = sta; id < end; id++)
-              {
-                  for(int jd = id + 1; jd < end; jd++)
-                  {
-                      int partI = planes[id].partID;
-                      int partJ = planes[jd].partID;
+    // 3) find all pairs of contact polygon
+    int sta = 0, end = 0;
+    vector<pairIJ> planeIJ;
+    while (sta < planes.size()) {
+        for (end = sta + 1; end < planes.size(); end++) {
+            if (planes[sta].groupID != planes[end].groupID) {
+                break;
+            }
+        }
+        if (end - sta > 1) {
+            for (int id = sta; id < end; id++) {
+                for (int jd = id + 1; jd < end; jd++) {
+                    int partI = planes[id].partID;
+                    int partJ = planes[jd].partID;
 
-                      Vector3 nrmI = planes[id].nrm.normalized();
-                      Vector3 nrmJ = planes[jd].nrm.normalized();
+                    Vector3 nrmI = planes[id].nrm.normalized();
+                    Vector3 nrmJ = planes[jd].nrm.normalized();
 
-                      if(partI != partJ
-                      && std::abs(nrmI.dot(nrmJ) + 1) < FLOAT_ERROR_LARGE
-                      && (!atBoundary[partI] || !atBoundary[partJ]))
-                          planeIJ.push_back(pairIJ(id, jd));
-                  }
-              }
-          }
-          sta = end;
-      }
+                    if (partI != partJ
+                        && std::abs(nrmI.dot(nrmJ) + 1) < FLOAT_ERROR_LARGE
+                        && (!atBoundary[partI] || !atBoundary[partJ]))
+                        planeIJ.push_back(pairIJ(id, jd));
+                }
+            }
+        }
+        sta = end;
+    }
 
 
-     // 4) parallel compute contacts
-     vector<pContactGraphEdge> planeIJEdges;
-     planeIJEdges.resize(planeIJ.size());
+    // 4) parallel compute contacts
+    vector<pContactGraphEdge> planeIJEdges;
+    planeIJEdges.resize(planeIJ.size());
 
-     tbb::parallel_for(tbb::blocked_range<size_t>(0, planeIJ.size()), [&](const tbb::blocked_range<size_t>& r)
-	 {
-         for (size_t id = r.begin(); id != r.end(); ++id)
-         // for (size_t id = 0; id != planeIJ.size(); ++id)
-	 	{
+    tbb::parallel_for(tbb::blocked_range<size_t>(0, planeIJ.size()), [&](const tbb::blocked_range<size_t> &r) {
+        for (size_t id = r.begin(); id != r.end(); ++id)
+            // for (size_t id = 0; id != planeIJ.size(); ++id)
+        {
 
-             int planeI = planeIJ[id].first;
-             int planeJ = planeIJ[id].second;
+            int planeI = planeIJ[id].first;
+            int planeJ = planeIJ[id].second;
 
-             vector<Vector3> polyI = planes[planeI].polygon.lock()->getVertices();
-             vector<Vector3> polyJ = planes[planeJ].polygon.lock()->getVertices();
-             vector<vector<Vector3>> contactPtLists;
+            vector<Vector3> polyI = planes[planeI].polygon.lock()->getVertices();
+            vector<Vector3> polyJ = planes[planeJ].polygon.lock()->getVertices();
+            vector<vector<Vector3>> contactPtLists;
 
-             PolyPolyBoolean<Scalar> ppIntersec(getVarList());
+            PolyPolyBoolean<Scalar> ppIntersec(getVarList());
 
-             ppIntersec.computePolygonsIntersection(polyI, polyJ, contactPtLists);
-             if (contactPtLists.empty())
-                 continue;
+            ppIntersec.computePolygonsIntersection(polyI, polyJ, contactPtLists);
+            if (contactPtLists.empty())
+                continue;
 
-             vector<pPolygon> contactPolys;
-             for(vector<Vector3> contactPtList: contactPtLists)
-             {
-                 if(contactPtList.size() >= 3){
-                     pPolygon contactPoly = make_shared<_Polygon<Scalar>>();
-                     contactPoly->setVertices(contactPtList);
-                     contactPolys.push_back(contactPoly);
-                 }
-             }
+            vector<pPolygon> contactPolys;
+            for (vector<Vector3> contactPtList: contactPtLists) {
+                if (contactPtList.size() >= 3) {
+                    pPolygon contactPoly = make_shared<_Polygon<Scalar>>();
+                    contactPoly->setVertices(contactPtList);
+                    contactPolys.push_back(contactPoly);
+                }
+            }
 
-             if(!contactPolys.empty()){
-                 planeIJEdges[id] = make_shared<ContactGraphEdge<Scalar>>(contactPolys, planes[planeI].nrm);
-             }
-         }
-     });
+            if (!contactPolys.empty()) {
+                planeIJEdges[id] = make_shared<ContactGraphEdge<Scalar>>(contactPolys, planes[planeI].nrm);
+            }
+        }
+    });
 
-     // 5) add contact edges
-     for(size_t id = 0; id < planeIJ.size(); id++)
-     {
-         pContactGraphEdge edge = planeIJEdges[id];
-         if(edge != nullptr){
-             int planeI = planeIJ[id].first;
-             int planeJ = planeIJ[id].second;
-             int partI = planes[planeI].partID;
-             int partJ = planes[planeJ].partID;
-             addContact(nodes[partI], nodes[partJ], edge);
-         }
-     }
+    // 5) add contact edges
+    for (size_t id = 0; id < planeIJ.size(); id++) {
+        pContactGraphEdge edge = planeIJEdges[id];
+        if (edge != nullptr) {
+            int planeI = planeIJ[id].first;
+            int planeJ = planeIJ[id].second;
+            int partI = planes[planeI].partID;
+            int partJ = planes[planeJ].partID;
+            addContact(nodes[partI], nodes[partJ], edge);
+        }
+    }
 
     return true;
 }
@@ -197,18 +186,14 @@ bool ContactGraph<Scalar>::constructFromPolyMeshes(vector<pPolyMesh> &meshes,
 *
 *************************************************/
 template<typename Scalar>
-void ContactGraph<Scalar>::addNode(pContactGraphNode _node)
-{
+void ContactGraph<Scalar>::addNode(pContactGraphNode _node) {
     _node->staticID = nodes.size();
 
     nodes.push_back(_node);
-
-    return;
 }
 
 template<typename Scalar>
-void ContactGraph<Scalar>::addContact(pContactGraphNode _nodeA, pContactGraphNode _nodeB, pContactGraphEdge _edge)
-{
+void ContactGraph<Scalar>::addContact(pContactGraphNode _nodeA, pContactGraphNode _nodeB, pContactGraphEdge _edge) {
 
     if (_nodeA->isBoundary && _nodeB->isBoundary)
         return;
@@ -227,44 +212,33 @@ void ContactGraph<Scalar>::addContact(pContactGraphNode _nodeA, pContactGraphNod
     contactNeighbor.first = _nodeA;
     contactNeighbor.second = _edge;
     _nodeB->neighbors.push_back(contactNeighbor);
-
-    return;
 }
 
 template<typename Scalar>
-void ContactGraph<Scalar>::finalize()
-{
+void ContactGraph<Scalar>::finalize() {
     int dynamicID = 0;
     dynamic_nodes.clear();
-    for (pContactGraphNode node : nodes)
-    {
-        if (!node->isBoundary)
-        {
+    for (pContactGraphNode node : nodes) {
+        if (!node->isBoundary) {
             node->dynamicID = dynamicID++;
             dynamic_nodes.push_back(node);
-        }
-        else
-        {
+        } else {
             node->dynamicID = -1;
         }
     }
 }
 
 template<typename Scalar>
-void ContactGraph<Scalar>::getContactMesh(pPolyMesh &mesh)
-{
+void ContactGraph<Scalar>::getContactMesh(pPolyMesh &mesh) {
     mesh.reset();
     mesh = make_shared<PolyMesh<Scalar>>(getVarList());
-    for(pContactGraphEdge edge: edges)
-    {
-        for(pPolygon poly: edge->polygons)
-        {
+    for (pContactGraphEdge edge: edges) {
+        for (pPolygon poly: edge->polygons) {
             mesh->polyList.push_back(poly);
         }
     }
 
-    if(mesh)
-        mesh->removeDuplicatedVertices();
+    if (mesh) mesh->removeDuplicatedVertices();
 
 //    mesh->ScaleMesh(1.0 / normalized_scale);
 //    mesh->TranslateMesh(-normalized_trans);
