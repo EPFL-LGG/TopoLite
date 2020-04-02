d0a8dad60928584443b49c3f341bb6b7007c2daf
rjodon <robin.jodon@epfl.ch>
2020-04-02 17:33:33 +0200

    More test cases for ContactGraph

--- test/Interlocking/Test_ContactGraph.cpp
+++ test/Interlocking/Test_ContactGraph.cpp
@@ -75,21 +75,21 @@ TEST_CASE("Class ContactGraph") {
         for (const auto &ver: graph->edges[0]->polygons[0]->vers) {
             contactPolygon.push_back(Vector3d(ver->pos[0], ver->pos[1], ver->pos[2]));
         }
-        REQUIRE(approximatelyEqual<double>(contactPolygon.area(), 1, 1e-4));
+        REQUIRE(approximatelyEqual<double>(contactPolygon.area(), 1, 1e-5));
     }
 
-    /*
-     *      |--<--|
-     *   |--|--| B|
-     *   |A |--|--|
-     *   |-->--|
-     */
-    SECTION("load two square A,B. A's normal is [0,0,1], B's normal is [0,0,1]") {
+    SECTION("load three squares A,B,C. A's normal is [0,0,1], B's normal is [0,0,-1], C's normal is [0,0,-1]") {
         pPolygon pB = make_shared<_Polygon<double>>();
         pB->push_back(Vector3d(1, 1, 0));
-        pB->push_back(Vector3d(3, 1, 0));
-        pB->push_back(Vector3d(3, 3, 0));
         pB->push_back(Vector3d(1, 3, 0));
+        pB->push_back(Vector3d(3, 3, 0));
+        pB->push_back(Vector3d(3, 1, 0));
+
+        pPolygon pC = make_shared<_Polygon<double>>();
+        pC->push_back(Vector3d(-1, -1, 0));
+        pC->push_back(Vector3d(-1,  1, 0));
+        pC->push_back(Vector3d( 1,  1, 0));
+        pC->push_back(Vector3d( 1, -1, 0));
 
         pPolyMesh meshA = make_shared<PolyMesh<double>>(varList);
         meshA->polyList.push_back(pA);
@@ -97,20 +97,103 @@ TEST_CASE("Class ContactGraph") {
         pPolyMesh meshB = make_shared<PolyMesh<double>>(varList);
         meshB->polyList.push_back(pB);
 
+        pPolyMesh meshC = make_shared<PolyMesh<double>>(varList);
+        meshC->polyList.push_back(pC);
+
         vector<pPolyMesh> meshes;
         meshes.push_back(meshA);
         meshes.push_back(meshB);
+        meshes.push_back(meshC);
 
         vector<bool> atBoundary;
+        atBoundary.push_back(false);
         atBoundary.push_back(false);
         atBoundary.push_back(false);
 
         shared_ptr<ContactGraph<double>> graph = make_shared<ContactGraph<double>>(varList);
         graph->buildFromMeshes(meshes, atBoundary);
 
-        REQUIRE(graph->nodes.size() == 2);
-        REQUIRE(graph->edges.size() == 0);
+        REQUIRE(graph->nodes.size() == 3);
+        REQUIRE(graph->edges.size() == 2);
 
+    }
+
+    SECTION("load three squares A,B,C. A's normal is [0,0,1], B's normal is [0,0,1], C's normal is [0,0,-1]") {
+        pPolygon pB = make_shared<_Polygon<double>>();
+        pB->push_back(Vector3d(1, 1, 0));
+        pB->push_back(Vector3d(3, 1, 0));
+        pB->push_back(Vector3d(3, 3, 0));
+        pB->push_back(Vector3d(1, 3, 0));
+
+        pPolygon pC = make_shared<_Polygon<double>>();
+        pC->push_back(Vector3d(-1, -1, 0));
+        pC->push_back(Vector3d(-1,  1, 0));
+        pC->push_back(Vector3d( 1,  1, 0));
+        pC->push_back(Vector3d( 1, -1, 0));
+
+        pPolyMesh meshA = make_shared<PolyMesh<double>>(varList);
+        meshA->polyList.push_back(pA);
+
+        pPolyMesh meshB = make_shared<PolyMesh<double>>(varList);
+        meshB->polyList.push_back(pB);
+
+        pPolyMesh meshC = make_shared<PolyMesh<double>>(varList);
+        meshC->polyList.push_back(pC);
+
+        vector<pPolyMesh> meshes;
+        meshes.push_back(meshA);
+        meshes.push_back(meshB);
+        meshes.push_back(meshC);
+
+        vector<bool> atBoundary;
+        atBoundary.push_back(false);
+        atBoundary.push_back(false);
+        atBoundary.push_back(false);
+
+        shared_ptr<ContactGraph<double>> graph = make_shared<ContactGraph<double>>(varList);
+        graph->buildFromMeshes(meshes, atBoundary);
+
+        REQUIRE(graph->nodes.size() == 3);
+        REQUIRE(graph->edges.size() == 1);
+    }
+
+    SECTION("load three squares A,B,C. A's normal is [0,0,1], B's normal is [0,0,1], C's normal is [0,0,1]") {
+        pPolygon pB = make_shared<_Polygon<double>>();
+        pB->push_back(Vector3d(1, 1, 0));
+        pB->push_back(Vector3d(3, 1, 0));
+        pB->push_back(Vector3d(3, 3, 0));
+        pB->push_back(Vector3d(1, 3, 0));
+
+        pPolygon pC = make_shared<_Polygon<double>>();
+        pC->push_back(Vector3d(-1, -1, 0));
+        pC->push_back(Vector3d( 1, -1, 0));
+        pC->push_back(Vector3d( 1,  1, 0));
+        pC->push_back(Vector3d(-1,  1, 0));
+
+        pPolyMesh meshA = make_shared<PolyMesh<double>>(varList);
+        meshA->polyList.push_back(pA);
+
+        pPolyMesh meshB = make_shared<PolyMesh<double>>(varList);
+        meshB->polyList.push_back(pB);
+
+        pPolyMesh meshC = make_shared<PolyMesh<double>>(varList);
+        meshC->polyList.push_back(pC);
+
+        vector<pPolyMesh> meshes;
+        meshes.push_back(meshA);
+        meshes.push_back(meshB);
+        meshes.push_back(meshC);
+
+        vector<bool> atBoundary;
+        atBoundary.push_back(false);
+        atBoundary.push_back(false);
+        atBoundary.push_back(false);
+
+        shared_ptr<ContactGraph<double>> graph = make_shared<ContactGraph<double>>(varList);
+        graph->buildFromMeshes(meshes, atBoundary);
+
+        REQUIRE(graph->nodes.size() == 3);
+        REQUIRE(graph->edges.empty());
     }
 
     SECTION("load two square A,B. A's normal is [0,0,1], B's normal is [0,0,1] but centor at Z:-0.0004") {
@@ -143,13 +226,46 @@ TEST_CASE("Class ContactGraph") {
 
         _Polygon<double> contactPolygon;
 
-        if (graph->edges.empty())
-            return;
+        for (const auto& ver: graph->edges[0]->polygons[0]->vers) {
+            contactPolygon.push_back(Vector3d(ver->pos[0], ver->pos[1], ver->pos[2]));
+        }
+        REQUIRE(approximatelyEqual<double>(contactPolygon.area(), 1, 1e-5));
+    }
+
+    SECTION("load two square A,B. A's normal is [0,0,1], B's normal is [0,0,-1] but centor at Z:-0.0004") {
+
+        pPolygon pB = make_shared<_Polygon<double>>();
+        pB->push_back(Vector3d(1, 1, -0.0004));
+        pB->push_back(Vector3d(3, 1, -0.0004));
+        pB->push_back(Vector3d(3, 3, -0.0004));
+        pB->push_back(Vector3d(1, 3, -0.0004));
+
+        pPolyMesh meshA = make_shared<PolyMesh<double>>(varList);
+        meshA->polyList.push_back(pA);
+
+        pPolyMesh meshB = make_shared<PolyMesh<double>>(varList);
+        meshB->polyList.push_back(pB);
+
+        vector<pPolyMesh> meshes;
+        meshes.push_back(meshA);
+        meshes.push_back(meshB);
+
+        vector<bool> atBoundary;
+        atBoundary.push_back(false);
+        atBoundary.push_back(false);
+
+        shared_ptr<ContactGraph<double>> graph = make_shared<ContactGraph<double>>(varList);
+        graph->buildFromMeshes(meshes, atBoundary);
+
+        REQUIRE(graph->nodes.size() == 2);
+        REQUIRE(graph->edges.empty());
+
+        _Polygon<double> contactPolygon;
 
         for (const auto& ver: graph->edges[0]->polygons[0]->vers) {
             contactPolygon.push_back(Vector3d(ver->pos[0], ver->pos[1], ver->pos[2]));
         }
-        REQUIRE(approximatelyEqual<double>(contactPolygon.area(), 1, 1e-4));
+        REQUIRE(approximatelyEqual<double>(contactPolygon.area(), 1, 1e-5));
     }
 
     SECTION("both A B at boundary") {
