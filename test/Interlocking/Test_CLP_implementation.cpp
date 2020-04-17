2866aefe316636ca70d0ba186e50197141b582a3
rjodon <robin.jodon@epfl.ch>
2020-04-17 19:09:28 +0200

    Fix matrix (col, row inversed)

--- test/Interlocking/Test_CLP_implementation.cpp
+++ test/Interlocking/Test_CLP_implementation.cpp
@@ -15,6 +15,19 @@ typedef Eigen::SparseMatrix<double> SpMat;
 typedef Eigen::Triplet<double> T;
 typedef Eigen::Matrix<double, 3, 1> Vector3d;
 
+/**
+ * @brief Simple test case of CLP simplex solver
+ *        We are solving the following LP problem
+ *
+ *        max 3x + 4y + 2z
+ *    s.t.    2x           >= 4     E1
+ *             x      + 2z >= 8     E2
+ *                 3y +  z >= 6     E3
+ *             (x,y,z)     >= 0     E4
+ *
+ *        Solution is: x,y,z = (2,1,3)
+ *                     max   = 16
+ */
 TEST_CASE("Simple case - SolveSimplex") {
     // Mock
     shared_ptr<InputVarList> varList = make_shared<InputVarList>();
@@ -22,7 +35,7 @@ TEST_CASE("Simple case - SolveSimplex") {
     shared_ptr<ContactGraph<double>> dat = make_shared<ContactGraph<double>>(varList);
     InterlockingSolver_Clp<double> solver(dat, varList);
     shared_ptr<typename InterlockingSolver<double>::InterlockingData> data;
-
+    //
     int num_col = 3;
     int num_row = 3;
     int num_var = 3;
@@ -35,9 +48,9 @@ TEST_CASE("Simple case - SolveSimplex") {
     std::vector<T> coef;
     //
     coef.emplace_back(T(0, 0, 2));
-    coef.emplace_back(T(0, 1, 1));
-    coef.emplace_back(T(2, 1, 2));
-    coef.emplace_back(T(1, 2, 3));
+    coef.emplace_back(T(1, 0, 1));
+    coef.emplace_back(T(1, 2, 2));
+    coef.emplace_back(T(2, 1, 3));
     coef.emplace_back(T(2, 2, 1));
 
     A.setFromTriplets(coef.begin(), coef.end());
@@ -63,7 +76,7 @@ TEST_CASE("Simple case - SolveSimplex") {
     for (size_t id = 0; id < num_row; id++) {
         constraintLower[id] = -COIN_DBL_MAX;
         constraintUpper[id] = rhs[id];
-        objective[id] = -objective_function[id];
+        objective[id] = -objective_function[id];  // simplex minimises by default
         printf("%zu %E %E %E \n", id, constraintLower[id], constraintUpper[id], objective[id]);
     }
 
@@ -74,7 +87,7 @@ TEST_CASE("Simple case - SolveSimplex") {
     model.passInMessageHandler(&handler);
     model.newLanguage(CoinMessages::us_en);
     model.loadProblem(matrix, varLower, varUpper, objective, constraintLower, constraintUpper);
-    model.setPrimalTolerance(1e-5);
+    model.setPrimalTolerance(1e-15);
     model.primal();
     // Get the solution
     const double obj_solution = model.rawObjectiveValue();
@@ -82,10 +95,11 @@ TEST_CASE("Simple case - SolveSimplex") {
     double *row_solution = model.primalRowSolution();
 
     for (size_t id = 0; id <num_var; id++)
-        printf("x[%zu] = %E\n", id+1, solution[id]);
+        printf("x%zu = %E\n", id+1, solution[id]);
     for (size_t id = 0; id < num_row; id++)
-        printf("E[%zu] = %E\n", id+1, row_solution[id]);
+        printf("E%zu = %E\n", id+1, row_solution[id]);
 
-
+    printf("Optimal?    %d \n", model.isProvenOptimal());
+    printf("Infeasable? %d \n", model.isProvenPrimalInfeasible());
     printf("Optimization - done");
 }
